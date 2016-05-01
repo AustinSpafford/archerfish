843d0c3af5dd6ffe2938041d11f7774054a37117
Austin Spafford <austin.spafford@gmail.com>
2016-05-01 14:07:13 -0700

    Basic nested sub-spheres.
    Admitting temporary defeat, there's a basic additive-shader that just
    renders a stack of transparent spheres. It _really_ needs to instead be
    rendering separate layers in multiple passes!

---
+++ Assets/Archerfish/HyperRender/HyperCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HyperCamera : MonoBehaviour
+{
+	public int SpaceSliceCount = 3;
+
+	public GameObject CameraPrefab = null;
+
+	public void Start()
+	{
+		for (int sliceIndex = 0;
+			sliceIndex < SpaceSliceCount;
+			++sliceIndex)
+		{
+			GameObject sliceCameraObject = GameObject.Instantiate(CameraPrefab);
+
+			Camera sliceCameraComponent = sliceCameraObject.GetComponent<Camera>();
+
+			RenderTexture sliceTexture = 
+				new RenderTexture(
+					sliceCameraComponent.pixelWidth,
+					sliceCameraComponent.pixelHeight,
+					depth: 24);
+
+			sliceCameraComponent.targetTexture = sliceTexture;
+		}
+	}
+}
+
